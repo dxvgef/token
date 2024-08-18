ef747b7ed87dc3bd7e74136f9c132be048a8fd9a
dxvgef <dxvgef@outlook.com>
2024-08-18 21:39:24 +0800

    加入SetAccessTokenPrefix和SetRefreshTokenPrefix

--- redis.go
+++ redis.go
@@ -7,8 +7,10 @@ import (
 const redisCliErr = "redis client is nil"
 
 var redisCli *redis.Client
+var accessTokenPrefix = ""
+var refreshTokenPrefix = ""
 
-// UseRedisClient 使用传入Redis客户端的连接
+// UseRedisClient 使用传入redis客户端的连接
 func UseRedisClient(cli *redis.Client) {
 	if cli == nil {
 		redisCli = redis.NewClient(&redis.Options{})
@@ -17,10 +19,20 @@ func UseRedisClient(cli *redis.Client) {
 	}
 }
 
-// NewRedisClient 使用新的Redis客户端连接
+// NewRedisClient 使用新的redis客户端连接
 func NewRedisClient(opts *redis.Options) {
 	if opts == nil {
 		opts = &redis.Options{}
 	}
 	redisCli = redis.NewClient(opts)
+}
+
+// SetAccessTokenPrefix 设置access token的redis键名前缀
+func SetAccessTokenPrefix(value string) {
+	accessTokenPrefix = value
+}
+
+// SetRefreshTokenPrefix 设置refresh token的redis键名前缀
+func SetRefreshTokenPrefix(value string) {
+	refreshTokenPrefix = value
 }
